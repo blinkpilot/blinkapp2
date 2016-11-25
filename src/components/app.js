fb8d81f8f1fe0d398af6307f05bc10014d3caf2f
Olga Sizova <olisizova@gmail.com>
2016-11-24 22:15:57 -0500

    saved app.js file

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,20 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Change me</h1>
+        <div class="main-container">
+          <div class="logo-container">
+          <div class="logo"></div>
+          <span id="logo-text"> Blink </span>
+  </div>
+
+
+  <div class="sign-in">
+  <span>
+  <a href="https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&response_type=code&client_id=491020538989-p7rt3a2bpl8qloj56qedsd327q5sg10p.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Flogged%2F"> <div id="login-button">Sign In</div> </a></span>
+
+
+  </div>
+</div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 import './styles/app.scss'
+import './styles/landing.css'
+import './styles/normalize.css'
+import 'https://fonts.googleapis.com/css?family=Baloo+Bhaina|Open+Sans'
 
 render(<App/>, document.getElementById('main'))
